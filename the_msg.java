1468a1a860145312f858150be29859c40bda115a
tay_ma_alda <temyhey@gmail.com>
2024-06-23 15:12:56 +0200

    it's all clean now :3 i've also edited the print methods to seem cooler

--- the_msg.java
+++ the_msg.java
@@ -1,43 +1,3 @@
-/*
-Import project.system.subsystems.you;
-
-// WARNING: read smartly!
-Mood mood = new Mood();
-Status status = new Status();
-Hope hope = new Hope();
-Age age = new Age();
-String OpmodeIsActive = you.isAlive();
-
-Boolean birthday = false;
-If(date.is(30.04) {
-    birthday = true;
-} else {
-    birthday= false;
-}
-
-While(OpmodeIsActive) {
-      mood.make.happy();
-      If(birthday) {
-          for(i, i<=1000, i++) {
-              System.out.println("( sqrt((HB)^2) + sqrt((D^2) ) + to/u u^2 ");
-            age.update(+1);
-          }
-      }
-      if(mood.sad() || status.tired() || hope.low()) {
-          mood.make.happy(forever);
-          status.make.energetic(forever);
-          hope.update(full);
-      }
-}
-
-!You are not perfect enough to change the world. !I do not believe in you.
-
-System.out.println("I hope you're smiling now as you're done reading this, and I hope you liked it :3");
-System.out.println("29th level 🥁");
-
-Report: if you find any issues or errors please report at temyhey@gmail.com or via Discord at tayma_aldahmani.
-
-*/
 
 import java.time.LocalDate;
 
@@ -49,7 +9,6 @@ public class the_msg {
         String mood;
         String status;
         String hope;
-        boolean alive = you.alive;
 
         age = 29;
         // TODO: input your status down here:
@@ -62,32 +21,29 @@ public class the_msg {
 
         double infinity = Double.POSITIVE_INFINITY;
 
-        // while ((boolean) you.isAlive()) {
-            
+        if ((boolean) you.isAlive()) {
+            if (currenDate.equals(bd)) {
+                for (int i = 0; i<=infinity; i++) {
+                    System.out.println("( sqrt((HB)^2) + sqrt((D^2) ) + to/u u^2 ");
+                }   
+            }
+    
+            if (mood.equalsIgnoreCase("sad") || status.equalsIgnoreCase("exhausted") || hope.equalsIgnoreCase("low")) {
+                you.makeHappy();
+                you.getEnergetic();
+                you.highHopes();
+                age ++;
+    
+                mood = you.mood;
+                status = you.status;
+                hope = you.hope;
 
-        // }
-
-        if (currenDate.equals(bd)) {
-            for (int i = 0; i<=infinity; i++) {
-                System.out.println("( sqrt((HB)^2) + sqrt((D^2) ) + to/u u^2 ");
+                System.out.println("you are " +mood);
+                System.out.println("and also " +status);
+                System.out.println("you have " +hope+ " hopes");
+                System.out.println("and you have just reached level... *drums* " +age+ " !!");
             }   
-        }
 
-        if (mood.equalsIgnoreCase("sad") || status.equalsIgnoreCase("exhausted") || hope.equalsIgnoreCase("low")) {
-            you.makeHappy();
-            you.getEnergetic();
-            you.highHopes();
-            age ++;
-
-            mood = you.mood;
-            status = you.status;
-            hope = you.hope;
-            // age = you.age;
-            
-            System.out.println(mood);
-            System.out.println(status);
-            System.out.println(hope);
-            System.out.println(age);
         }
 
         
